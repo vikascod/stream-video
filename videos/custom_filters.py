40a78fd12cd4d624a1e943f3e27c5c36da475adf
Vikas <vikas1618072@gmail.com>
2023-06-06 08:17:25 +0530

    add(update requirements.txt): :rocket: update

--- videos/custom_filters.py
+++
@@ -1,8 +0,0 @@
-from django import template
-from videos.models import SaveVideo
-
-register = template.Library()
-
-@register.filter
-def is_saved(video, user):
-    return SaveVideo.objects.filter(video=video, user=user).exists()
